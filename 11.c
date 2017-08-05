24687148637be26d172117bf60b02ca8b3f92830
karuppusamy1010 <30342568+karuppusamy1010@users.noreply.github.com>
2017-08-05 16:50:21 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int b, e;
+
+    long long result = 1;
+
+    printf("Enter a base number: ");
+    scanf("%d", &b);
+
+    printf("Enter an exponent: ");
+    scanf("%d", &e);
+
+    while (e != 0)
+    {
+        result *= b;
+        --e;
+    }
+
+    printf("Answer = %lld", result);
+
+    return 0;
+}
